913569ca4ce2f899c63a40c02d376b7e282a0348
Benjamin Halbert <bhalbert2@mail.greenriver.edu>
2020-05-13 20:43:50 -0400

    added styling and more content

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ error_reporting(E_ALL);
 
 //Require autoload
 require_once('vendor/autoload.php');
+require_once("model/data-layer.php");
 
 session_start();
 
@@ -26,5 +27,46 @@ $f3->route('GET /', function() {
     echo $view->render('views/home.html');
 });
 
+
+$f3->route('GET|POST /personalInformation', function($f3) {
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        var_dump($_POST);
+
+        $_SESSION['FirstName'] = $_POST(['FirstName']);
+        $_SESSION['LastName'] = $_POST(['lastName']);
+        $_SESSION['Age'] = $_POST(['Age']);
+        $_SESSION['Gender'] = $_POST(['Gender']);
+        $_SESSION['Number'] = $_POST(['Number']);
+    }
+    $view = new View;
+    echo $view->render('views/personalInformation.html');
+});
+$f3->route('GET|POST /profile', function($f3) {
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $_SESSION['Email'] = $_POST(['Email']);
+        $_SESSION['State'] = $_POST(['State']);
+        $_SESSION['Seeking'] = $_POST(['Seeking']);
+        $_SESSION['Biography'] = $_POST(['Biography']);
+    }
+    $view = new View;
+    echo $view->render('views/profile.html');
+});
+
+$f3->route('GET /Interests', function($f3) {
+    $InInterests = getInInterests();
+    $OutInterests = getOutInterests();
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $_SESSION['InDoor'] = $_POST(['InDoor']);
+        $_SESSION['OutDoor'] = $_POST(['OutDoor']);
+    }
+    $f3->set('InInterests', $InInterests);
+    $f3->set('OutInterests', $OutInterests);
+    $view = new View;
+    echo $view->render('views/interests.html');
+});
+$f3->route('GET /Summary', function() {
+    $view = new View;
+    echo $view->render('views/summary.html');
+});
 //Run fat free
 $f3->run();
---
+++ model/data-layer.php
@@ -0,0 +1,10 @@
+<?php
+
+function getInInterests(){
+    return $InInterests = array("TV", "Movies", "Cooking", "Board Games", "Puzzles", "Reading",
+        "Playing Cards", "Video Games");
+}
+
+function getOutInterests(){
+    return $InInterests = array("Hiking", "Biking", "Swimming", "Collecting", "Walking", "Climbing");
+}
